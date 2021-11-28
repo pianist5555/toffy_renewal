c12350205c375285a6328d6d27bdfff89b9decb8
Seono <pianist5555@gmail.com>
2021-11-28 10:24:39 +0900

    'struct'

--- front/front/src/pages/Index.js
+++ front/front/src/pages/Index.js
@@ -7,14 +7,34 @@ class Index extends Component {
   render() {
     return (
       <div className="Index">
-        <div className="Toffy">
-          <a href="/toffy">
-            <img src={Toffy} alt="Toffy" />
-            
-          </a>
-        </div>
         <div className="Rocket">
           <img src={Rocket} alt="Rocket"  width="300"/>
+        </div>
+        <div className="Profile">
+          프로필
+        </div>
+        <div className="Carrer">
+          경력사항
+        </div>
+        <div className="Carrer">
+          핵심역량
+        </div>
+        <div className="Carrer">
+          보유기술
+        </div>
+        <div className="Carrer">
+          자격사항
+        </div>
+        <div className="Carrer">
+          사이드프로젝트
+          <div className="Toffy">
+            <a href="/toffy">
+              <img src={Toffy} alt="Toffy" />
+            </a>
+          </div>
+        </div>
+        <div className="Carrer">
+          자기소개서
         </div>
       </div>
     );
